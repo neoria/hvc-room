7ac229f26c611401716f7f04807fdfcbb2187ec2
neoria <hvcharu@gmail.com>
2019-12-08 16:41:41 +0900

    トップページを作成

---
+++ global/header.js
@@ -0,0 +1,17 @@
+; (_ => {
+document.write(`
+<header>
+  <nav class="navbar navbar-expand-sm navbar-dark cyan">
+    <a class="navbar-brand" href="/hvc-room/">
+      ひぶしの部屋
+    </a>
+    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId"
+      aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavId">
+    </div>
+  </nav>
+</header>
+`);
+})();
