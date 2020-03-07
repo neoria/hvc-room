3089dee6f2bca06865e03d723e7dd959584c68f1
neoria <hvcharu@gmail.com>
2020-03-07 19:02:31 +0900

    jsからjsをappendChildで読み込むテスト

--- global/pageTemplateBottom.js
+++ global/pageTemplateBottom.js
@@ -24,11 +24,30 @@ A parser - blocking, cross site(i.e.different eTLD + 1) script, <URL>, is invoke
 
   <!-- Material Design Bootstrap ------------------------------ -->
   <!-- JQuery, Bootstrap tooltips, Bootstrap core JavaScript, MDB core JavaScript -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
 </body>
 </html>
   `);
+  let hoge = document.createElement("script");
+  // hoge.textContent = "alert(123)";
+  hoge.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js");
+  document.body.appendChild(hoge);
+  hoge = document.createElement("script");
+  hoge.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js");
+  document.body.appendChild(hoge);
+  hoge = document.createElement("script");
+  hoge.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js");
+  document.body.appendChild(hoge);
+  hoge = document.createElement("script");
+  hoge.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js");
+  document.body.appendChild(hoge);
+
+  
 })();
+// {/* <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+//   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+//   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+//   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script> */}
+
+
+
+
